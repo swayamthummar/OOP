6d5bfa16d0c57527258cb91d87f3723b53833bcf
swayam thummar <127782240+swayamthummar@users.noreply.github.com>
2023-04-24 17:03:34 +0530

    Add files via upload

---
+++ OOP-1.cpp
@@ -0,0 +1,122 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+class students
+{
+	public:
+	int roll;
+	char name[100];
+	int std;
+	char contact[12];
+	char ad[100];
+};
+main()
+{
+	students s1,s2,s3,s4,s5;
+	
+	cout<<"Enter Roll Number->";
+	cin>>s1.roll;
+	cout<<"Enter Name->";
+	cin>>s1.name;
+	cout<<"Enter standard->";
+	cin>>s1.std;
+	cout<<"Enter Contact Deails->";
+	cin>>s1.contact;
+	cout<<"Enter Address->";
+	cin>>s1.ad;
+	
+	cout<<endl;
+	
+	cout<<"Enter Roll Number->";
+	cin>>s2.roll;
+	cout<<"Enter Name->";
+	cin>>s2.name;
+	cout<<"Enter standard->";
+	cin>>s2.std;
+	cout<<"Enter Contact Deails->";
+	cin>>s2.contact;
+	cout<<"Enter Address->";
+	cin>>s2.ad;
+	
+	cout<<endl;
+	
+		cout<<"Enter Roll Number->";
+	cin>>s3.roll;
+	cout<<"Enter Name->";
+	cin>>s3.name;
+	cout<<"Enter standard->";
+	cin>>s3.std;
+	cout<<"Enter Contact Deails->";
+	cin>>s3.contact;
+	cout<<"Enter Address->";
+	cin>>s3.ad;
+	
+	cout<<endl;
+	
+		cout<<"Enter Roll Number->";
+	cin>>s4.roll;
+	cout<<"Enter Name->";
+	cin>>s4.name;
+	cout<<"Enter standard->";
+	cin>>s4.std;
+	cout<<"Enter Contact Deails->";
+	cin>>s4.contact;
+	cout<<"Enter Address->";
+	cin>>s4.ad;
+	
+	cout<<endl;
+	
+		cout<<"Enter Roll Number->";
+	cin>>s5.roll;
+	cout<<"Enter Name->";
+	cin>>s5.name;
+	cout<<"Enter standard->";
+	cin>>s5.std;
+	cout<<"Enter Contact Deails->";
+	cin>>s5.contact;
+	cout<<"Enter Address->";
+	cin>>s5.ad;
+	
+	cout<<"--------------------------------------------"<<endl;
+	
+	cout<<"Roll number ->"<<s2.roll<<endl;
+	cout<<"Name ->"<<s2.name<<endl;
+	cout<<"Standard ->"<<s2.std<<endl;
+	cout<<"Contact Detail ->"<<s2.contact<<endl;
+	cout<<"Address ->"<<s2.ad<<endl;
+	
+	cout<<endl;
+	
+	cout<<"Roll number ->"<<s3.roll<<endl;
+	cout<<"Name ->"<<s3.name<<endl;
+	cout<<"Standard ->"<<s3.std<<endl;
+	cout<<"Contact Detail ->"<<s3.contact<<endl;
+	cout<<"Address ->"<<s3.ad<<endl;
+	
+	cout<<endl;
+	
+	cout<<"Roll number ->"<<s4.roll<<endl;
+	cout<<"Name ->"<<s4.name<<endl;
+	cout<<"Standard ->"<<s4.std<<endl;
+	cout<<"Contact Detail ->"<<s4.contact<<endl;
+	cout<<"Address ->"<<s4.ad<<endl;
+	
+	cout<<endl;
+	
+	cout<<"Roll number ->"<<s4.roll<<endl;
+	cout<<"Name ->"<<s4.name<<endl;
+	cout<<"Standard ->"<<s4.std<<endl;
+	cout<<"Contact Detail ->"<<s4.contact<<endl;
+	cout<<"Address ->"<<s4.ad<<endl;
+	
+	cout<<endl;
+	
+	cout<<"Roll number ->"<<s5.roll<<endl;
+	cout<<"Name ->"<<s5.name<<endl;
+	cout<<"Standard ->"<<s5.std<<endl;
+	cout<<"Contact Detail ->"<<s5.contact<<endl;
+	cout<<"Address ->"<<s5.ad<<endl;
+	
+	cout<<endl;
+}
+
