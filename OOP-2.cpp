ecc791902510aeb8fc32ae0eea2cfa581c9157c3
swayam thummar <127782240+swayamthummar@users.noreply.github.com>
2023-04-24 21:37:29 +0530

    Add files via upload

---
+++ OOP-2.cpp
@@ -0,0 +1,121 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+class employ
+{
+	public:
+	int id;
+	char name[100];
+	char company[100];
+	char salary[100];
+	char ad[100];
+};
+main()
+{
+	employ e1,e2,e3,e4,e5;
+	
+	cout<<"Enter ID->";
+	cin>>e1.id;
+	cout<<"Enter Name->";
+	cin>>e1.name;
+	cout<<"Enter Company Name->";
+	cin>>e1.company;
+	cout<<"Enter Salary->";
+	cin>>e1.salary;
+	cout<<"Enter Address->";
+	cin>>e1.ad;
+	
+	cout<<endl;
+	
+cout<<"Enter ID->";
+	cin>>e2.id;
+	cout<<"Enter Name->";
+	cin>>e2.name;
+	cout<<"Enter Company Name->";
+	cin>>e2.company;
+	cout<<"Enter Salary->";
+	cin>>e2.salary;
+	cout<<"Enter Address->";
+	cin>>e2.ad;
+	
+	cout<<endl;
+	
+		cout<<"Enter ID->";
+	cin>>e3.id;
+	cout<<"Enter Name->";
+	cin>>e3.name;
+	cout<<"Enter Company Name->";
+	cin>>e3.company;
+	cout<<"Enter Salary->";
+	cin>>e3.salary;
+	cout<<"Enter Address->";
+	cin>>e3.ad;
+	
+	cout<<endl;
+	
+		cout<<"Enter ID->";
+	cin>>e4.id;
+	cout<<"Enter Name->";
+	cin>>e4.name;
+	cout<<"Enter Company Name->";
+	cin>>e4.company;
+	cout<<"Enter Salary->";
+	cin>>e4.salary;
+	cout<<"Enter Address->";
+	cin>>e4.ad;
+	
+	cout<<endl;
+	
+		cout<<"Enter ID->";
+	cin>>e5.id;
+	cout<<"Enter Name->";
+	cin>>e5.name;
+	cout<<"Enter Company Name->";
+	cin>>e5.company;
+	cout<<"Enter Salary->";
+	cin>>e5.salary;
+	cout<<"Enter Address->";
+	cin>>e5.ad;
+		
+	cout<<"--------------------------------------------"<<endl;
+	
+	cout<<"ID ->"<<e1.id<<endl;
+	cout<<"Name ->"<<e1.name<<endl;
+	cout<<"Company ->"<<e1.company<<endl;
+	cout<<"Salary ->"<<e1.salary<<endl;
+	cout<<"Address ->"<<e1.ad<<endl;
+	
+	cout<<endl;
+	
+	cout<<"ID ->"<<e2.id<<endl;
+	cout<<"Name ->"<<e2.name<<endl;
+	cout<<"Company ->"<<e2.company<<endl;
+	cout<<"Salary ->"<<e2.salary<<endl;
+	cout<<"Address ->"<<e2.ad<<endl;
+	
+	cout<<endl;
+	
+	cout<<"ID ->"<<e3.id<<endl;
+	cout<<"Name ->"<<e3.name<<endl;
+	cout<<"Company ->"<<e3.company<<endl;
+	cout<<"Salary ->"<<e3.salary<<endl;
+	cout<<"Address ->"<<e3.ad<<endl;
+	
+	cout<<endl;
+	
+	cout<<"ID ->"<<e4.id<<endl;
+	cout<<"Name ->"<<e4.name<<endl;
+	cout<<"Company ->"<<e4.company<<endl;
+	cout<<"Salary ->"<<e4.salary<<endl;
+	cout<<"Address ->"<<e4.ad<<endl;
+		
+	cout<<endl;
+	
+	cout<<"ID ->"<<e5.id<<endl;
+	cout<<"Name ->"<<e5.name<<endl;
+	cout<<"Company ->"<<e5.company<<endl;
+	cout<<"Salary ->"<<e5.salary<<endl;
+	cout<<"Address ->"<<e5.ad<<endl;
+	
+	cout<<endl;
+}
